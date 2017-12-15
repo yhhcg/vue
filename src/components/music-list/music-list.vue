5b27a5f7804aef24df8b5e1104c8a1db5f402424
yheng <1281005689@qq.com>
2017-12-15 23:20:31 +0800

    优化music－list组件

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title">{{title}}</h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" v-show="songs.length>0" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -18,6 +24,9 @@
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
@@ -25,6 +34,7 @@
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
   import SongList from 'base/song-list/song-list'
+  import Loading from 'base/loading/loading'
 
   const RESERVED_HEIGHT = 40
 
@@ -65,6 +75,9 @@
     methods: {
       scroll(pos) {
         this.scrollY = pos.y
+      },
+      back() {
+        this.$router.back()
       }
     },
     watch: {
@@ -82,24 +95,28 @@
         } else {
           blur = Math.min(20 * percent, 20)
         }
-        // 解决title处显示列表问题
+        // true为滑动到顶部 解决title处显示列表问题
         if (this.minTranslateY > newY) {
           zIndex = 10
           this.$refs.bgImage.style['paddingTop'] = 0
           this.$refs.bgImage.style['height'] = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
         } else {
           this.$refs.bgImage.style['paddingTop'] = '70%'
           this.$refs.bgImage.style['height'] = 0
+          this.$refs.playBtn.style.display = ''
         }
         this.$refs.bgImage.style.zIndex = zIndex
         this.$refs.bgImage.style['transform'] = `scale(${scale})`
         this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
         this.$refs.filter.style['backdrop-filter'] = `blur(${blur})` // 高斯模糊
+        this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur})`
       }
     },
     components: {
       Scroll,
-      SongList
+      SongList,
+      Loading
     }
   }
 </script>
