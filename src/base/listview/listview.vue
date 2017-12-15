29aabe96d909de2e1f53f8c7b327c8039308c9fa
yheng <1281005689@qq.com>
2017-12-15 09:56:34 +0800

    优化listview组件

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -3,13 +3,14 @@
             :data="data"
             ref="listview"
             :listenScroll="listenScroll"
+            :probeType="probeType"
             @scroll="scroll"
     >
       <ul>
         <li v-for="group in data" class="list-group" ref="listGroup">
           <h2 class="list-group-title">{{group.title}}</h2>
           <ul>
-            <li v-for="item in group.items" class="list-group-item">
+            <li @click="selectItem(item)" v-for="item in group.items" class="list-group-item">
               <img class="avatar" v-lazy="item.avatar">
               <span class="name">{{item.name}}</span>
             </li>
@@ -18,17 +19,25 @@
       </ul>
       <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
         <ul>
-          <li class="item" v-for="(item,index) in shortCutList" :class="{current: currentPageIndex === index}" :data-index="index">{{item}}</li>
+          <li class="item" v-for="(item,index) in shortCutList" :class="{current: currentIndex === index}" :data-index="index">{{item}}</li>
         </ul>
+      </div>
+      <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+        <h1 class="fixed-title">{{fixedTitle}}</h1>
+      </div>
+      <div v-show="!data.length" class="loading-container">
+        <loading></loading>
       </div>
     </scroll>
 </template>
 
 <script>
     import Scroll from 'base/scroll/scroll'
+    import Loading from 'base/loading/loading'
     import {getData} from 'common/js/dom'
 
     const ANCHOR_HEIGHT = 18
+    const TITLE_HEIGHT = 30
 
     export default {
       props: {
@@ -39,20 +48,29 @@
       },
       data() {
         return {
-          scrollY: 0
+          scrollY: 0,
+          currentIndex: 0, // 当前list-group-item的索引
+          diff: -1
         }
       },
       created() {
         this.touch = {}
         this.listenScroll = true
-        this.currentPageIndex = 0 // 当前list-group-item的索引
         this.listHeight = [] // 所有list-group-item的高度
+        this.probeType = 3
       },
       computed: {
         shortCutList() {
           return this.data.map((group) => {
             return group.title.substr(0, 1)
           })
+        },
+        fixedTitle() {
+          // data = []
+          if (this.scrollY > 0) {
+            return ''
+          }
+          return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
         }
       },
       methods: {
@@ -69,8 +87,10 @@
           this._scrollTo(anchorIndex)
         },
         scroll(pos) {
-          console.log(pos)
           this.scrollY = pos.y
+        },
+        selectItem(item) {
+          this.$emit('select', item)
         },
         _calculateHeight() {
           this.listHeight = []
@@ -83,6 +103,18 @@
           }
         },
         _scrollTo(index) {
+          // 点击缩写的头部和尾部  padding
+          if (!index && index !== 0) {
+            return
+          }
+          // 拖动到顶
+          if (index < 0) {
+            index = 0
+            // 拖动到底
+          } else if (index > this.listHeight.length - 2) {
+            index = this.listHeight.length - 2
+          }
+          this.scrollY = -this.listHeight[index]
           this.$refs.listview.scrollToElement(this.$refs.listGroup[index])
         }
       },
@@ -92,20 +124,34 @@
             this._calculateHeight()
           }, 20)
         },
-        scroll(newVal) {
+        scrollY(newVal) {
+          let listHeight = this.listHeight
+          // 滚动到顶部
           if (newVal > 0) {
-            this.currentPageIndex = 0
-            return false
+            this.currentIndex = 0
+            return
           }
-          for (let i = 0; i < this.listHeight.length; i++) {}
-          if (newVal > -this.listHeight[this.listHeight.length - 2]) {
-            this.currentPageIndex = this.listHeight.length - 2
-            return false
+          // 中间滚动
+          for (let i = 0; i < listHeight.length - 1; i++) {
+            let height1 = listHeight[i]
+            let height2 = listHeight[i + 1]
+            if (-newVal >= height1 && -newVal < height2) {
+              this.currentIndex = i
+              this.diff = height2 + newVal // listgroupItem剩余的高度
+              return
+            }
           }
+          // 滚动到底部
+          this.currentIndex = listHeight.length - 2
+        },
+        diff(newVal) {
+          let fixedTop = newVal < TITLE_HEIGHT ? newVal - TITLE_HEIGHT : 0
+          this.$refs.fixed.style.transform = `translate3d(0, ${fixedTop}px, 0)`
         }
       },
       components: {
-        Scroll
+        Scroll,
+        Loading
       }
     }
 </script>
